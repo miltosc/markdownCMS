832fa6ed36bbd6b72d9142ce20cbf421387d2542
Gregory Kappa <gk@makemagic.gr>
2016-06-01 22:26:13 +0300

    draft - better config for initial setup

--- web-app/main_files/config_demo_favorites_file.php
+++ web-app/main_files/config_demo_favorites_file.php
@@ -1,12 +1,19 @@
-<?php  $favorites_files_r_txt=array (
-  'settings' => 
+<?php
+// path for app to work with the demo content OR
+// path to work with file-folders inside the app root folder
+$path_to_app_root = dirname(dirname(dirname(__FILE__)));
+// comment previous line and uncomment the line bellow to define a files-folder at the server document root folder level
+// $path_to_app_root = $_SERVER['DOCUMENT_ROOT'];
+
+$favorites_files_r_txt=array (
+  'settings' =>
   array (
     'default_fav' => 0,
   ),
-  'fav_files' => 
+  'fav_files' =>
   array (
-    0 => $_SERVER['DOCUMENT_ROOT']."/markdown/README.md",
-    1 => $_SERVER['DOCUMENT_ROOT']."/markdown/demo-md-files/notes2.md",
-    2 => $_SERVER['DOCUMENT_ROOT']."/markdown/demo-md-files/notes3.txt",
+    0 => $path_to_app_root."/README.md",
+    1 => $path_to_app_root."/demo-md-files/notes2.md",
+    2 => $path_to_app_root."/demo-md-files/notes3.txt",
   ),
 ); ?>
